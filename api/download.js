6f042ff5cfdac3f442ec1c26d3f3d77b2b0f6924
liyao231381 <2313814221@qq.com>
2025-03-25 23:29:08 +0800

    5

--- api/download.js
+++ api/download.js
@@ -7,10 +7,11 @@ module.exports = (req, res) => {
         return res.status(400).send('Missing video URL');
     }
 
-    const filename = videoUrl.substring(videoUrl.lastIndexOf('/') + 1);
+    const filename = "抖音下载.mp4";
+    const encodedFilename = encodeURIComponent(filename);
 
     // 设置响应头，告诉浏览器这是一个下载文件
-    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+    res.setHeader('Content-Disposition', `attachment; filename="${encodedFilename}"`);
     res.setHeader('Content-Type', 'video/mp4'); // 根据实际视频格式修改
 
     // 使用 'request' 模块将视频流式传输到响应
